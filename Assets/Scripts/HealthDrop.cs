731a37b4d6187a4ce2582ac496352596b03ff1d2
Anthony Konschuh <silv.amee@gmail.com>
2021-09-10 13:25:05 -0700

    Add health animation and health pickup animation

--- Assets/Scripts/HealthDrop.cs
+++ Assets/Scripts/HealthDrop.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class HealthDrop : Drop
 {
     [SerializeField] private int HealthAmount;
@@ -8,10 +9,12 @@ public class HealthDrop : Drop
     [SerializeField] private float spawnForceMult;
 
     private Rigidbody2D rb2d;
+    private Animator anim;
 
     private void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 
         var forceVector = new Vector2(Random.Range(-spawnXMagnitude, spawnXMagnitude), 1);
         rb2d.AddForce(forceVector * spawnForceMult, ForceMode2D.Impulse);
@@ -21,4 +24,13 @@ public class HealthDrop : Drop
     {
         player.Heal(HealthAmount);
     }
+
+    public override void DestroyDrop()
+    {
+        anim.SetTrigger("t_pickup");
+
+        Invoke("Destroy", 1);
+    }
+
+    private void Destroy () => base.DestroyDrop();
 }
