48e720567d6acdf11c6ede7f223f3e8d4cb8efb7
Anthony Konschuh <silv.amee@gmail.com>
2021-09-12 17:44:47 -0700

    Add fire ball explosion and mana bubbles

--- Assets/Scripts/Fireball.cs
+++ Assets/Scripts/Fireball.cs
@@ -2,7 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Fireball : Projectile
 {
-    
+    private Animator anim;
+
+    public override void Start()
+    {
+        base.Start();
+
+        anim = GetComponent<Animator>();
+    }
+
+    public override void OnCollisionEnter2D(Collision2D collision)
+    {
+        direction = 0;
+        base.OnCollisionEnter2D(collision);
+        gameObject.layer = 9;
+    }
+
+    public override void HandleHitObject()
+    {
+        anim.SetTrigger("t_hitObject");
+
+        Invoke("Destroy", 1);
+    }
+
+    public void Destroy()
+    {
+        base.HandleHitObject();
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,24 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : Character
 {
+    [Header("Magic Details")]
+    public int maxMana;
     public Projectile projectile;
+    [SerializeField] private GameObject manaUiBox;
+    [SerializeField] private Sprite filledManaBubble;
+    [SerializeField] private Sprite emptyManaBubble;
+
+    private int currentMana;
 
     public override void Start()
     {
         base.Start();
+
+        currentMana = maxMana;
+        
+        RenderMana();
     }
 
     public override void Update()
@@ -33,12 +45,59 @@ public class Player : Character
 
     private void FireProjectile()
     {
-        if (projectile)
+        if (projectile && currentMana > 0)
         {
+            currentMana--;
+
             var proj = Instantiate(projectile, attackPoint.position, attackPoint.rotation);
             proj.direction = transform.localScale.x;
             proj.enemyLayer = enemyLayer;
+
+            UpdateManaUI();
         }
+    }
+
+    private void RenderMana()
+    {
+        var halfImageWidth = 10;
+        var imageRightPadding = 5;
+
+        for(var i = 0; i < maxMana; i++)
+        {
+            var anchorX = halfImageWidth + (i * halfImageWidth * 2) + (i * imageRightPadding);
+            CreateManaImage(new Vector2(anchorX, -halfImageWidth));
+        }
+
+        UpdateManaUI();
+    }
+
+    private void UpdateManaUI()
+    {
+        for(var i = 0; i < maxMana; i++)
+        {
+            var sprite = i <= currentMana - 1 ? filledManaBubble : emptyManaBubble;
+            manaUiBox.transform.GetChild(i).GetComponent<Image>().sprite = sprite;
+        }
+    }
+
+    private Image CreateManaImage(Vector2 anchor)
+    {
+        var bubble = new GameObject("ManaBubble", typeof(Image));
+
+        bubble.transform.SetParent(manaUiBox.transform);
+        bubble.transform.localPosition = Vector3.zero;
+        bubble.transform.localScale = new Vector3(0.2f, 0.2f);
+
+        var rect = bubble.GetComponent<RectTransform>();
+
+        rect.anchorMin = new Vector2(0, 1);
+        rect.anchorMax = new Vector2(0, 1);
+        rect.anchoredPosition = anchor;
+
+        var bubbleImage = bubble.GetComponent<Image>();
+        bubbleImage.sprite = emptyManaBubble;
+
+        return bubbleImage;
     }
 
     public void Heal(int amount)
